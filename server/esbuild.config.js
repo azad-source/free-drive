81f8bd972350b3dd9d8638041dc06c3d4e9038dc
Azad MAMEDOV <mamedov.a@skbkontur.ru>
2024-08-11 18:51:18 +0400

    test

---
+++ server/esbuild.config.js
@@ -0,0 +1,13 @@
+import esbuild from "esbuild";
+
+esbuild
+  .build({
+    entryPoints: ["src/server.ts"],
+    bundle: true,
+    platform: "node",
+    target: "node20",
+    outfile: "dist/server.js",
+    minify: true, // Минимизирует бандл
+    // external: ["ws"],
+  })
+  .catch(() => process.exit(1));
