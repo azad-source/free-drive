38186d5c69216d6ae3609b3781b496b80aef4e2f
Azad MAMEDOV <mamedov.a@skbkontur.ru>
2024-08-14 02:47:18 +0400

    add cors origin

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,12 @@ const server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
 });
 
 const io = geckos({
