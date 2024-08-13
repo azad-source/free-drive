aab04e0f54ca7a13e10e6840717ddeee7835c1a5
Azad MAMEDOV <mamedov.a@skbkontur.ru>
2024-08-14 02:37:56 +0400

    add cors origin

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ import http from "http";
 type IGame = Record<string, any>;
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "m-azad-.ru");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 });
