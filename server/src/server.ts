362c731d1be3e4a37ae95160c74fef49e4c33842
Azad MAMEDOV <mamedov.a@skbkontur.ru>
2024-08-14 16:23:11 +0400

    test

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,22 +3,15 @@ import http from "http";
 
 type IGame = Record<string, any>;
 
+const origin = "http://m-azad.ru";
+
 const server = http.createServer((req, res) => {
-  // const origin1 = req.headers.origin;
-  const origin2 = "http://m-azad.ru";
-  res.setHeader("Access-Control-Allow-Origin", origin2);
-  // res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  // if (req.method === "OPTIONS") {
-  //   res.writeHead(204);
-  //   res.end();
-  //   return;
-  // }
+  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 });
 
-const io = geckos({
-  // cors: { allowAuthorization: true, origin: "http://m-azad.ru:80" },
-});
+const io = geckos({ cors: { origin } });
 
 io.addServer(server);
 
