80cf5e1b3f50ad8ec77169638aa819900ed13967
Azad MAMEDOV <mamedov.a@skbkontur.ru>
2024-08-19 01:01:51 +0400

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { terser } from "rollup-plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+  input: "src/server.ts",
+  output: {
+    file: "dist/server.js",
+    format: "esm",
+  },
+  plugins: [
+    typescript(),
+    resolve(), // Разрешение модулей из node_modules
+    commonjs(), // Преобразование CommonJS модулей в ES6
+    json(), // Поддержка импорта JSON файлов
+    terser(), // Минификация кода
+  ],
+};
