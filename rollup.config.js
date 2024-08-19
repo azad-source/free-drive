83eb8973f21148632e61258115ff0cdc86edbf10
Azad MAMEDOV <mamedov.a@skbkontur.ru>
2024-08-19 23:34:08 +0400

    add rollup (fix)

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,12 @@ import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import { terser } from "rollup-plugin-terser";
 import typescript from "@rollup/plugin-typescript";
+import copy from "rollup-plugin-copy";
 
 export default {
   input: "src/server.ts",
   output: {
-    file: "dist/server.js",
+    file: "dist/main/server.js",
     format: "es",
   },
   plugins: [
@@ -16,6 +17,13 @@ export default {
     commonjs(), // Преобразование CommonJS модулей в ES6
     json(), // Поддержка импорта JSON файлов
     terser(), // Минификация кода
+    copy({
+      targets: [
+        {
+          src: "node_modules/node-datachannel/build/Release/node_datachannel.node",
+          dest: "dist/build/Release",
+        },
+      ],
+    }),
   ],
-  external: ["../build/Release/node_datachannel.node"],
 };
