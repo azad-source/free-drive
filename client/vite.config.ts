1af8f762c3e8f83e306cdd0d6181fa4f131489d7
Azad MAMEDOV <mamedov.a@skbkontur.ru>
2024-08-20 00:09:28 +0400

    add copy plugon to vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig, loadEnv } from "vite";
 // import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 // import svgr from "vite-plugin-svgr";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [viteTsconfigPaths()],
+    plugins: [
+      viteTsconfigPaths(),
+      viteStaticCopy({
+        targets: [{ src: "src/assets/models/car/*", dest: "assets" }],
+      }),
+    ],
     server: {
       open: true,
       port: 3000,
